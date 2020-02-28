fd4991c959e6417d3b845aa59e8f2d7599440dae
daemon-cool <damian.damian.tofik@gmail.com>
2020-02-28 10:55:28 +0100

    Added static resources on server lik scripts, styles

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 app = express();
+app.use(express.static('public'));
 
 app.listen(8000, () => {
     console.log(`Server running at http://${'127.0.0.1'}:${8000}/`);
