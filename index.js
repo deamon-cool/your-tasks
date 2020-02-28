76ebc75bb15bde0ec016e2f65bd00a3f1f6b808f
daemon-cool <damian.damian.tofik@gmail.com>
2020-02-28 11:14:08 +0100

    Created initial server respond with view tempate index.edge

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const {engine} = require('express-edge');
 
 app = express();
 app.use(express.static('public'));
+
+app.use(engine);
+app.set('views', `${__dirname}/views`);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 app.listen(8000, () => {
     console.log(`Server running at http://${'127.0.0.1'}:${8000}/`);
