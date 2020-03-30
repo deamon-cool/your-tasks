a95309b02669501cf23775d7d85d3d73afec49e0
daemon-cool <damian.damian.tofik@gmail.com>
2020-03-30 15:06:32 +0200

    Added comment. Things that should be finished in the future

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,2 @@
-// when task will be dragover set box-shadow: 0 5px 6px var(--general-blue-color); to better look
-// later turn off this
+// 1. when task will be dragover set box-shadow: 0 5px 6px var(--general-blue-color); to better look later turn off this
+// 2. Add real time that measure state fnishing tasks . P.s. It will be more powerfull if you add progress bar in task in FRONTEND
