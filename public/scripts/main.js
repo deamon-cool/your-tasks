91263192b601420a9b76bbe4d8a9b6bfe8cb8e6a
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-01 10:39:27 +0200

    To do in future

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,2 +1,3 @@
 // 1. when task will be dragover set box-shadow: 0 5px 6px var(--general-blue-color); to better look later turn off this
-// 2. Add real time that measure state fnishing tasks . P.s. It will be more powerfull if you add progress bar in task in FRONTEND
+// 2. Add real time that measure state fnishing tasks. (FUTURE)P.s. It will be more powerfull if you add progress bar in task in FRONTEND
+// 3. In menu task add one more option Copy (FUTURE)
