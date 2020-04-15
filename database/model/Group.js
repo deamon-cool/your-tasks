da01b0440b84fc82ab1cad5103ab2e68830e9bc8
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-15 15:51:24 +0200

    Corrected

--- database/model/Group.js
+++ database/model/Group.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const GroupSchema = mongoose.Schema({
     position: Number,
     name: String,
-    listsId: [mongoose.Types.ObjectId]
+    listIds: [mongoose.Types.ObjectId]
 });
 
 const Group = mongoose.model('Groups', GroupSchema);
