878f9d4752b062aeb83ab4f0f2b33dc70a6f0859
daemon-cool <damian.damian.tofik@gmail.com>
2020-04-16 12:24:08 +0200

    Implemented

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Group = require('./database/model/Group');
+const List = require('./database/model/List');
 
 
 // Database connection
