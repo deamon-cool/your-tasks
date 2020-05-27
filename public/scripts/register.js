c5e6d00a08a2d50ea02a9bf2bba5a549f54341e5
daemon-cool <damian.damian.tofik@gmail.com>
2020-05-27 17:48:30 +0200

    added register html content

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,5 +1,22 @@
 let container = document.querySelector('#container');
 
 container.innerHTML = `
+<div class="window">
+    <h1>Register:</h1>
 
+    <form action="/register/store/user" method="POST">
+        <label for="username">User Name:</label>
+        <input type="text" name="username" placeholder="User Name">
+
+        <label for="password">Password:</label>
+        <input type="password" name="password" placeholder="Password">
+
+        <label for="confirm">Confirm Password:</label>
+        <input type="password" name="confirm" placeholder="Confirm Password">
+
+        <input type="submit" value="✔">
+    </form>
+
+    <p><a href="/register/privacy">Privacy</a></p>
+</div>
 `;
