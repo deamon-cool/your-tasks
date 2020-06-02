a403a62bfa41cf26e73216a5dd45b3c806bd68c8
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-02 14:25:28 +0200

    added basic validation

--- middleware/registerValidation.js
+++ middleware/registerValidation.js
@@ -1,6 +1,10 @@
 module.exports = async (req, res, next) => {
+    if (req.body.username === undefined || req.body.password === undefined
+        || req.body.username === '' || req.body.password === '') {
+        return res.redirect('/register');
+    }
 
 
 
     next();
-}
+};
