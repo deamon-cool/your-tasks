eb1129cb50d21b6bbef5d28ae0eeefb07dd1b43d
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-09 18:33:45 +0200

    created test for storing activation codes in Db with expire function

---
+++ tests/createTestActivationCodes.js
@@ -0,0 +1,20 @@
+//---------------------------TEST deleting test Db-------------------------------
+
+const mongoose = require('mongoose');
+const ActivationCode = require('../database/model/ActivationCode');
+
+// Database connection
+mongoose.connect('mongodb://localhost/your-tasks-db', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error :|'));
+db.once('open', () => {
+    console.log('test: connected to db :)');
+
+    createData().then(console.log('completed -> Collection ActivationCodes is filled :)'));
+});
+
+async function createData() {
+    await ActivationCode.create({
+        code: code
+    });
+}
