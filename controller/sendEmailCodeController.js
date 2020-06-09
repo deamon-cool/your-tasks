7fb2281e3833b1dbe1c8dffe9155b73744a9a7c6
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-09 11:52:42 +0200

    added skelteon for send email controller

--- controller/sendEmailCodeController.js
+++ controller/sendEmailCodeController.js
@@ -1,3 +1,14 @@
 module.exports = (req, res) => {
-    // send email
+    try {
+        let email = req.body.email;
+        // send email
+
+        console.log(`Sent email to ${email}`);
+
+        return res.send({ error: false, serverOutput: 'Check your email and type in your Activation Code' });
+    } catch (e) {
+        console.log('Err for: sendEmailCodeController ----------------------------------------------------->\n' + e);
+
+        return res.redirect(400, '/error');
+    }
 };
