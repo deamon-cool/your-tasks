085326c7cf17548d2af975aee0dffc540635f333
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-09 16:32:26 +0200

    added script that deleting whole db

--- tests/deleteDbData.js
+++ tests/deleteDbData.js
@@ -1,1 +1,17 @@
 //---------------------------TEST deleting test Db-------------------------------
+
+const mongoose = require('mongoose');
+
+// Database connection
+mongoose.connect('mongodb://localhost/your-tasks-db', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error :|'));
+db.once('open', () => {
+    console.log('test: connected to db :)');
+
+    deleteDb().then(console.log('completed -> Database is deleted :)'));
+});
+
+async function deleteDb() {
+    await mongoose.connection.db.dropDatabase('your-tasks-db');
+}
