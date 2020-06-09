9518d063567f484453976fd6a5f69a7bc332c8d9
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-09 17:07:54 +0200

    updated middleware to check email

--- middleware/emailCheck.js
+++ middleware/emailCheck.js
@@ -1,6 +1,26 @@
-module.exports = (req, res, next) => {
-    // email validation
-    // check email in db
+const emailValidator = require('email-validator');
+const User = require('../database/model/User');
 
-    next();
+module.exports = async (req, res, next) => {
+    try {
+        let email = req.body.email;
+        let state = emailValidator.validate(email);
+
+        if (state) {
+            const user = await User.findOne({ email: email });
+
+            if (user === null) {
+                next();
+            } else {
+                return res.send({ error: true, serverOutput: 'Email exists. You should type another Email' });
+            }
+
+        } else {
+            return res.send({ error: true, serverOutput: 'Wrong Email' });
+        }
+    } catch (e) {
+        console.log('Err for: emailCheck ----------------------------------------------------->\n' + e);
+
+        return res.redirect(400, '/error');
+    }
 };
