ca6a154e13ee4d8fa740784e67e68dbea57cd669
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-10 12:45:31 +0200

    added function to creating hashcodes from emails

--- tests/createTestDbActivationCodes.js
+++ tests/createTestDbActivationCodes.js
@@ -13,6 +13,24 @@ let emails = ['d@a.pl', 'd@w.eu', 'k@ap.ru'];
 
 console.log(`----------------> Your emails:\n${emails}`);
 
+// Creates hash codes
+async function createSampleCodes() {
+    for (let i = 0; i < emails.length; i++) {
+        const email = emails[i];
+
+        const hash = await bcrypt.hash(email, saltRounds);
+
+        hashCodes.push(hash);
+    }
+
+    await checkHashWithOriginals();
+
+    console.log(`----------------> Your emails hash codes:\n${hashCodes}`);
+
+    return Promise.resolve();
+}
+
+let sampleCodesPromise = createSampleCodes();
 // Database connection
 mongoose.connect('mongodb://localhost/your-tasks-db', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
