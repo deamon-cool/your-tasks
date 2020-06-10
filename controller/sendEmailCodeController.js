7cb770666b161b462e7bcedc16e9d9c24ae01a2a
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-10 17:01:58 +0200

    added functionality for sending email

--- controller/sendEmailCodeController.js
+++ controller/sendEmailCodeController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 const ActivationCode = require('../database/model/ActivationCode');
 
 module.exports = async (req, res) => {
@@ -12,11 +13,30 @@ module.exports = async (req, res) => {
             activationCode
         });
 
-        // send email
+        let transport = nodemailer.createTransport({
+            host: 'gmail',
+            secure: true,
+            port: 465,
+            auth: {
+                user: 'youremail@gmail.com',
+                pass: 'password'
+            }
+        });
 
-        console.log(`Sent email to ${email}`);
+        const message = {
+            from: 'youremail@gmail.com',
+            to: email,
+            subject: 'Copy Activation Code',
+            text: `$2b$10$TW7/Sz3E3zwlGrMW26AjzuRU4MS4pn6tkIIsLMs8AMPe2gFDgP6R2`
+        };
 
-        return res.send({ error: false, serverOutput: 'Check your email and type in your Activation Code' });
+        transport.sendMail(message, (err, info) => {
+            if (err) {
+                return res.send({ error: true, serverOutput: 'Cannot sant email' });
+            } else {
+                return res.send({ error: false, serverOutput: 'Check your email and type in your Activation Code' });
+            }
+        });
     } catch (e) {
         console.log('Err for: sendEmailCodeController ----------------------------------------------------->\n' + e);
 
