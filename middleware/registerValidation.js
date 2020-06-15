9ff4f3d2622629470324c4f81509402e0cfc5808
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-15 16:23:35 +0200

    updated register validation middleware

--- middleware/registerValidation.js
+++ middleware/registerValidation.js
@@ -1,10 +1,25 @@
+const emailValidator = require('email-validator');
+
 module.exports = async (req, res, next) => {
-    if (req.body.username === undefined || req.body.password === undefined
-        || req.body.username === '' || req.body.password === '') {
-        return res.redirect('/register');
-    }
+    try {
+        const username = req.body.username;
+        const email = req.body.email;
+        const code = req.body.code;
 
+        if (username === '' || email === '' || code === '' || req.body.password === '') {
+            return res.send({
+                error: true,
+                serverOutput: 'Fill out Username, Email, Activation Code, Password, Confirm Password'
+            });
+        }
 
+        if (emailValidator.validate(email)) {
+            return res.send({ error: true, serverOutput: 'Wrong Email' });
+        }
 
-    next();
-};
+        next();
+    } catch (e) {
+        console.log('Err for: registerValidation ----------------------------------------------------->\n' + e);
+
+        return res.redirect(400, '/error');
+    };
