b734322afc5d64234324bb35ce4167bd880edafa
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-15 18:31:57 +0200

    updated storing user controller. Added password hashing before saving

--- controller/storeUserController.js
+++ controller/storeUserController.js
@@ -1,6 +1,17 @@
+const bcrypt = require('bcrypt');
+const User = require('../database/model/User');
+
 module.exports = async (req, res) => {
     try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
+        await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        return res.redirect('/login');
     } catch (e) {
         console.log('Err for: storeUserController ----------------------------------------------------->\n' + e);
 
