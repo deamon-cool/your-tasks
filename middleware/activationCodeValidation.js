613f0ddfcca5149c8eb30cfe03b6816dfaa44259
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-15 18:09:17 +0200

    updated validation actiavation code middleware

--- middleware/activationCodeValidation.js
+++ middleware/activationCodeValidation.js
@@ -1,8 +1,26 @@
+const ActivationCode = require('../database/model/ActivationCode');
+
 module.exports = async (req, res, next) => {
-    console.log(req.body);
+    try {
+        const email = req.body.email;
+        const code = req.body.code;
 
-    
+        const activationCode = await ActivationCode.findOne({ email: email });
 
+        if (activationCode === null) {
+            return res.send({
+                error: true,
+                serverOutput: 'Correct your Email and try again. If it doesn\'t help, reload page and Send Code again' });
+        }
 
-    next();
+        if (activationCode.code !== code) {
+            return res.send({ error: true, serverOutput: 'Activation Code is incorrect' });
+        }
+
+        next();
+    } catch (e) {
+        console.log('Err for: activationCodeValidation ----------------------------------------------------->\n' + e);
+
+        return res.redirect(400, '/error');
+    }
 };
