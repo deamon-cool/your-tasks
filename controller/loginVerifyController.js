5b2dad8bd8401d99e0ff8c9506dc18fb94b0719d
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-19 18:39:22 +0200

    added full login controller

--- controller/loginVerifyController.js
+++ controller/loginVerifyController.js
@@ -3,9 +3,18 @@ const bcrypt = require('bcrypt');
 
 module.exports = async (req, res) => {
     try {
+        const user = await User.findOne({ email: req.body.email });
 
-        return res.send({ error: true, serverOutput: 'Could not log in' });
-        // return res.redirect(301, '/main');
+        if (user === null) {
+            return res.send({ error: true, serverOutput: 'Not found User' });
+        }
+
+        let passwordCorrect = await bcrypt.compare(req.body.password, user.password);
+        if (!passwordCorrect) {
+            return res.send({ error: true, serverOutput: 'Wrong Password' });
+        }
+
+        return res.redirect(301, '/main');
     } catch (e) {
         console.log('Err for: loginVerifyController ----------------------------------------------------->\n' + e);
 
