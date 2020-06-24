7cbc6f05e42226dc446a917edebad3c4bfbbeaf4
daemon-cool <damian.damian.tofik@gmail.com>
2020-06-24 18:44:09 +0200

    added module

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 // Models Db
 const User = require('./database/model/User');
